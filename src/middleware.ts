0c9d4df61737d13cd9a2f737129770495e957172
Mayur Kawale <122032765+Mefisto04@users.noreply.github.com>
2025-03-21 01:11:46 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import { jwtVerify } from "jose";
 export async function middleware(request: NextRequest) {
     // Allow public access to the home page ("/"), auth pages, and API auth routes
     if (
+        
         request.nextUrl.pathname === "/" ||
         request.nextUrl.pathname.startsWith("/auth") ||
         request.nextUrl.pathname.startsWith("/api/auth")
